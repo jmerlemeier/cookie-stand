7ff67c43ea6d1b3558d24c7fb794b438642c20ee
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-15 16:30:53 -0700

    Created 4th array containing string with hour and aver cookie sold.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 // Hour Array
 var hourArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
  
+// store my DOM node in a variable
+var ulEL = document.getElementById()
 // 1st and Pike Location
 var firstPike = {
   name: '1st and Pike',
@@ -15,15 +17,28 @@ var firstPike = {
       var random = Math.floor(Math.random() * (this.maxcust-this.mincust) + this.mincust);
       this.randcustArr.push(random);
     }
-    console.log(random)
+    console.log('I am inside randcust');
   },
   randcookieArr: [],
-  randCookies: function() {
-    for(var i = 0; i < randcustArr.length; i++){
-     this.randcookieArr.push(randcustArr[i]*this.avecookie);
+  randcookies: function() {
+    for(var i = 0; i < this.randcustArr.length; i++){
+     this.randcookieArr.push(Math.floor(this.randcustArr[i]*this.avecookie));
     }
+    console.log('I am inside randcookies');
+  },
+  unorderedArr: [],
+  unordered: function() {
+    for(var i = 0; i < hourArr.length; i++){
+      this.unorderedArr.push(`${hourArr[i]}: ${this.randcookieArr[i]} cookies`);
+    }
+    console.log('I am inside unorderedArr');
+    console.log(this.unorderedArr);
   }
 };
+
+firstPike.randcust();
+firstPike.randcookies();
+firstPike.unordered();
 
 // renderfunction()
 
