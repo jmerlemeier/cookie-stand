28ce642439a2b58908af64665d95b4cef4e7d46d
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-15 14:34:27 -0700

    Created files, Hour array, and skeleton of raw objects

---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// Hour Array
+var hourArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+ 
+// 1st and Pike Location
+var firstPike = {
+  mincust: 23, 
+  maxcust: 65,
+  avecookie: 6.3,
+  randomNum = Math.round(math.random()*(maxcust-mincust+1)+mincust),
+};
+// renderfunction()
+
+// SeaTac Airport
+var seaTac = {
+  mincust: 3, 
+  maxcust: 24,
+  avecookie: 1.2,
+  // methodtogeneratecookie: ,
+};
+// renderfunction()
+
+// Seattle Center
+var seattleCenter = {
+  mincust: 11, 
+  maxcust: 38,
+  avecookie: 3.7,
+  // methodtogeneratecookie: ,
+};
+// renderfunction()
+
+// Capitol Hill
+var CapitolHill = {
+  mincust: 20, 
+  maxcust: 38,
+  avecookie: 2.3,
+  // methodtogeneratecookie: ,
+};
+// renderfunction()
+
+// Alki
+var alki = {
+  mincust: 2, 
+  maxcust: 16,
+  avecookie: 4.6,
+  // methodtogeneratecookie: ,
+};
+// renderfunction()
