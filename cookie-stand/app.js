b2bcb21cabc710ae374036816c8916040029c2f2
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-15 21:07:57 -0700

    Made onjects for remaining 4 stores using same foundation from ststore 1.

--- cookie-stand/app.js
+++ cookie-stand/app.js
@@ -2,12 +2,13 @@
 
 //Hour Array
 var hourArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
-// Total Cookies
-
  
 //Store my DOM node in a variable
-var ulEl = document.getElementById('pink-cookie-list');
+var ulEl = document.getElementById('pike-cookie-list');
+var ulEl = document.getElementById('seatac-cookie-list');
+var ulEl = document.getElementById('seattlecenter-cookie-list');
+var ulEl = document.getElementById('capitolhill-cookie-list');
+var ulEl = document.getElementById('alki-cookie-list');
 
 //1st and Pike Location
 var firstPike = {
@@ -24,21 +25,20 @@ var firstPike = {
       var random = Math.floor(Math.random() * (this.maxcust-this.mincust) + this.mincust);
       this.randcustArr.push(random);
     }
-    console.log('I am inside randcust');
+    console.log('I am inside 1standPike-randcust');
   },
   randcookies: function() {
     for(var i = 0; i < this.randcustArr.length; i++){
      this.randcookieArr.push(Math.floor(this.randcustArr[i]*this.avecookie));
      this.cookieTotal += (this.randcookieArr[i]);
     }
-    console.log('I am inside randcookies');
-    // console.log(cookieTotal);
+    console.log('I am inside 1standPike-randcookies');
   },
   unordered: function() {
     for(var i = 0; i < hourArr.length; i++){
       this.unorderedArr.push(`${hourArr[i]}: ${this.randcookieArr[i]} cookies`);
     }
-    console.log('I am inside unorderedArr');
+    console.log('I am inside 1standPike-unorderedArr');
     console.log(this.unorderedArr);
   },
   render: function(){
@@ -49,49 +49,199 @@ var firstPike = {
       var liEl = document.createElement('li');
       liEl.textContent = this.unorderedArr[i];
       ulEl.appendChild(liEl);
-      
     }
     var liEl = document.createElement('li');
     liEl.textContent = `TOTAL: ${this.cookieTotal} cookies`;
     ulEl.appendChild(liEl);
   }
 };
-// Rendering Functions/Executable Code
 // renderfunction()
 firstPike.render();
-
 //--------------------SeaTac Airport---------------------------
 var seaTac = {
+  name: 'SeaTac',
   mincust: 3, 
   maxcust: 24,
   avecookie: 1.2,
-  // methodtogeneratecookie: ,
+  cookieTotal: 0,
+  randcustArr: [],
+  randcookieArr: [],
+  unorderedArr: [],
+  randcust: function() {
+    for(var i  = 0; i < hourArr.length; i++){
+      var random = Math.floor(Math.random() * (this.maxcust-this.mincust) + this.mincust);
+      this.randcustArr.push(random);
+    }
+    console.log('I am inside SeaTac-randcust');
+  },
+  randcookies: function() {
+    for(var i = 0; i < this.randcustArr.length; i++){
+     this.randcookieArr.push(Math.floor(this.randcustArr[i]*this.avecookie));
+     this.cookieTotal += (this.randcookieArr[i]);
+    }
+    console.log('I am inside SeaTac-randcookies');
+  },
+  unordered: function() {
+    for(var i = 0; i < hourArr.length; i++){
+      this.unorderedArr.push(`${hourArr[i]}: ${this.randcookieArr[i]} cookies`);
+    }
+    console.log('I am inside SeaTac-unorderedArr');
+    console.log(this.unorderedArr);
+  },
+  render: function(){
+    this.randcust();
+    this.randcookies();
+    this.unordered();
+    for(var i=0; i < hourArr.length; i++){
+      var liEl = document.createElement('li');
+      liEl.textContent = this.unorderedArr[i];
+      ulEl.appendChild(liEl);
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = `TOTAL: ${this.cookieTotal} cookies`;
+    ulEl.appendChild(liEl);
+  }
 };
-// renderfunction()
-
+// Render Function
+seaTac.render();
 //--------------------Seattle Center---------------------------
 var seattleCenter = {
+  name: 'Seattle Center',
   mincust: 11, 
   maxcust: 38,
   avecookie: 3.7,
-  // methodtogeneratecookie: ,
+  cookieTotal: 0,
+  randcustArr: [],
+  randcookieArr: [],
+  unorderedArr: [],
+  randcust: function() {
+    for(var i  = 0; i < hourArr.length; i++){
+      var random = Math.floor(Math.random() * (this.maxcust-this.mincust) + this.mincust);
+      this.randcustArr.push(random);
+    }
+    console.log('I am inside Seattlecenter-randcust');
+  },
+  randcookies: function() {
+    for(var i = 0; i < this.randcustArr.length; i++){
+     this.randcookieArr.push(Math.floor(this.randcustArr[i]*this.avecookie));
+     this.cookieTotal += (this.randcookieArr[i]);
+    }
+    console.log('I am inside Seattlecenter-randcookies');
+  },
+  unordered: function() {
+    for(var i = 0; i < hourArr.length; i++){
+      this.unorderedArr.push(`${hourArr[i]}: ${this.randcookieArr[i]} cookies`);
+    }
+    console.log('I am inside seattlecenter-unorderedArr');
+    console.log(this.unorderedArr);
+  },
+  render: function(){
+    this.randcust();
+    this.randcookies();
+    this.unordered();
+    for(var i=0; i < hourArr.length; i++){
+      var liEl = document.createElement('li');
+      liEl.textContent = this.unorderedArr[i];
+      ulEl.appendChild(liEl);
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = `TOTAL: ${this.cookieTotal} cookies`;
+    ulEl.appendChild(liEl);
+  }
 };
 // renderfunction()
-
+seattleCenter.render();
 //--------------------Capitol Hill---------------------------
-var CapitolHill = {
+var capitolHill = {
+  name: 'CapitolHill',
   mincust: 20, 
   maxcust: 38,
   avecookie: 2.3,
-  // methodtogeneratecookie: ,
+  cookieTotal: 0,
+  randcustArr: [],
+  randcookieArr: [],
+  unorderedArr: [],
+  randcust: function() {
+    for(var i  = 0; i < hourArr.length; i++){
+      var random = Math.floor(Math.random() * (this.maxcust-this.mincust) + this.mincust);
+      this.randcustArr.push(random);
+    }
+    console.log('I am inside CapitolHill-randcust');
+  },
+  randcookies: function() {
+    for(var i = 0; i < this.randcustArr.length; i++){
+     this.randcookieArr.push(Math.floor(this.randcustArr[i]*this.avecookie));
+     this.cookieTotal += (this.randcookieArr[i]);
+    }
+    console.log('I am inside CapitolHill-randcookies');
+  },
+  unordered: function() {
+    for(var i = 0; i < hourArr.length; i++){
+      this.unorderedArr.push(`${hourArr[i]}: ${this.randcookieArr[i]} cookies`);
+    }
+    console.log('I am inside CapitolHill-unorderedArr');
+    console.log(this.unorderedArr);
+  },
+  render: function(){
+    this.randcust();
+    this.randcookies();
+    this.unordered();
+    for(var i=0; i < hourArr.length; i++){
+      var liEl = document.createElement('li');
+      liEl.textContent = this.unorderedArr[i];
+      ulEl.appendChild(liEl);
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = `TOTAL: ${this.cookieTotal} cookies`;
+    ulEl.appendChild(liEl);
+  }
 };
 // renderfunction()
-
+capitolHill.render();
 //-------------------------Alki------------------------------
 var alki = {
+  name: 'Alki',
   mincust: 2, 
   maxcust: 16,
   avecookie: 4.6,
-  // methodtogeneratecookie: ,
+  cookieTotal: 0,
+  randcustArr: [],
+  randcookieArr: [],
+  unorderedArr: [],
+  randcust: function() {
+    for(var i  = 0; i < hourArr.length; i++){
+      var random = Math.floor(Math.random() * (this.maxcust-this.mincust) + this.mincust);
+      this.randcustArr.push(random);
+    }
+    console.log('I am inside Alki-randcust');
+  },
+  randcookies: function() {
+    for(var i = 0; i < this.randcustArr.length; i++){
+     this.randcookieArr.push(Math.floor(this.randcustArr[i]*this.avecookie));
+     this.cookieTotal += (this.randcookieArr[i]);
+    }
+    console.log('I am inside Alki-randcookies');
+  },
+  unordered: function() {
+    for(var i = 0; i < hourArr.length; i++){
+      this.unorderedArr.push(`${hourArr[i]}: ${this.randcookieArr[i]} cookies`);
+    }
+    console.log('I am inside Alki-unorderedArr');
+    console.log(this.unorderedArr);
+  },
+  render: function(){
+    this.randcust();
+    this.randcookies();
+    this.unordered();
+    for(var i=0; i < hourArr.length; i++){
+      var liEl = document.createElement('li');
+      liEl.textContent = this.unorderedArr[i];
+      ulEl.appendChild(liEl);
+    }
+    var liEl = document.createElement('li');
+    liEl.textContent = `TOTAL: ${this.cookieTotal} cookies`;
+    ulEl.appendChild(liEl);
+  }
 };
 // renderfunction()
+alki.render();
