41d96f9294d72ffbaec5ddf5bda903ea4bbef56b
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-15 17:43:56 -0700

    Rendered Functions: created li, gave content, appended to the COM.

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 'use strict';
 
-// Hour Array
+//Hour Array
 var hourArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
  
-// store my DOM node in a variable
-var ulEL = document.getElementById()
-// 1st and Pike Location
+//Store my DOM node in a variable
+var ulEl = document.getElementById('cookie-list');
+
+//1st and Pike Location
 var firstPike = {
   name: '1st and Pike',
   mincust: 23, 
   maxcust: 65,
   avecookie: 6.3,
   randcustArr: [],
+  randcookieArr: [],
+  unorderedArr: [],
+  cookieTotal: 0,
   randcust: function() {
     for(var i  = 0; i < hourArr.length; i++){
       var random = Math.floor(Math.random() * (this.maxcust-this.mincust) + this.mincust);
@@ -19,30 +23,35 @@ var firstPike = {
     }
     console.log('I am inside randcust');
   },
-  randcookieArr: [],
   randcookies: function() {
     for(var i = 0; i < this.randcustArr.length; i++){
      this.randcookieArr.push(Math.floor(this.randcustArr[i]*this.avecookie));
     }
     console.log('I am inside randcookies');
   },
-  unorderedArr: [],
   unordered: function() {
     for(var i = 0; i < hourArr.length; i++){
       this.unorderedArr.push(`${hourArr[i]}: ${this.randcookieArr[i]} cookies`);
     }
     console.log('I am inside unorderedArr');
     console.log(this.unorderedArr);
+  },
+  render: function(){
+    this.randcust();
+    this.randcookies();
+    this.unordered();
+    for(var i=0; i < hourArr.length; i++){
+      var liEl = document.createElement('li');
+      liEl.textContent = this.unorderedArr[i];
+      ulEl.appendChild(liEl); 
+    }
   }
 };
-
-firstPike.randcust();
-firstPike.randcookies();
-firstPike.unordered();
-
+// Rendering Functions/Executable Code
 // renderfunction()
+firstPike.render();
 
-// SeaTac Airport
+//--------------------SeaTac Airport---------------------------
 var seaTac = {
   mincust: 3, 
   maxcust: 24,
