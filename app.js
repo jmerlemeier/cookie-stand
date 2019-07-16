0b0dce7997d278369c1d0d1f482790dea0d5f7ae
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-15 18:25:47 -0700

    Made totals for Cookies.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 //Hour Array
 var hourArr = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// Total Cookies
+
  
 //Store my DOM node in a variable
 var ulEl = document.getElementById('cookie-list');
@@ -12,10 +15,10 @@ var firstPike = {
   mincust: 23, 
   maxcust: 65,
   avecookie: 6.3,
+  cookieTotal: 0,
   randcustArr: [],
   randcookieArr: [],
   unorderedArr: [],
-  cookieTotal: 0,
   randcust: function() {
     for(var i  = 0; i < hourArr.length; i++){
       var random = Math.floor(Math.random() * (this.maxcust-this.mincust) + this.mincust);
@@ -26,8 +29,10 @@ var firstPike = {
   randcookies: function() {
     for(var i = 0; i < this.randcustArr.length; i++){
      this.randcookieArr.push(Math.floor(this.randcustArr[i]*this.avecookie));
+     this.cookieTotal += (this.randcookieArr[i]);
     }
     console.log('I am inside randcookies');
+    // console.log(cookieTotal);
   },
   unordered: function() {
     for(var i = 0; i < hourArr.length; i++){
@@ -43,8 +48,12 @@ var firstPike = {
     for(var i=0; i < hourArr.length; i++){
       var liEl = document.createElement('li');
       liEl.textContent = this.unorderedArr[i];
-      ulEl.appendChild(liEl); 
+      ulEl.appendChild(liEl);
+      
     }
+    var liEl = document.createElement('li');
+    liEl.textContent = `TOTAL: ${this.cookieTotal} cookies`;
+    ulEl.appendChild(liEl);
   }
 };
 // Rendering Functions/Executable Code
@@ -60,7 +69,7 @@ var seaTac = {
 };
 // renderfunction()
 
-// Seattle Center
+//--------------------Seattle Center---------------------------
 var seattleCenter = {
   mincust: 11, 
   maxcust: 38,
@@ -69,7 +78,7 @@ var seattleCenter = {
 };
 // renderfunction()
 
-// Capitol Hill
+//--------------------Capitol Hill---------------------------
 var CapitolHill = {
   mincust: 20, 
   maxcust: 38,
@@ -78,7 +87,7 @@ var CapitolHill = {
 };
 // renderfunction()
 
-// Alki
+//-------------------------Alki------------------------------
 var alki = {
   mincust: 2, 
   maxcust: 16,
